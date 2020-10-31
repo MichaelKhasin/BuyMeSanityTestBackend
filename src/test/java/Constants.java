dec6a104bbf1e1b48611b6496388680da294fbe1
MichaelKhasin <69919586+MichaelKhasin@users.noreply.github.com>
2020-10-31 23:24:38 +0200

    Update Constants.java

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 class Constants{
     public static final String MY_EMAIL = "khasin13@gmail.com";
-    public static final String MY_PASSWORD = "Tomer1noam";
+    public static final String MY_PASSWORD = "*****";
     //public static final By MY_LOCATOR = By.id("John");
     public static final String USER_NAME = "hQm6Iojltz";
     public static final String DATABASE_NAME = "hQm6Iojltz";
